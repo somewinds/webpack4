fa8194bdbffe258d2e6b951c4c930b604deb4479
liuyajun <liuyajun@linhuiba.com>
2019-02-19 11:17:29 +0800

    配置devServer npm run server

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -20,5 +20,18 @@ module.exports = {
   // 插件：用于生产模块和各项功能
   plugins: [],
   // 配置webpack开发服务功能，如 热模块更新作用hot
-  devServer: {}
+  devServer: {
+    // contentBase 设置基本目录结构
+    // 你要提供哪里的内容给虚拟服务器用。这里最好填 绝对路径。
+    // 默认情况下，它将使用您当前的工作目录来提供内容。
+    contentBase: path.resolve(__dirname, '../dist'),
+    // host 服务器的IP地址，可以使用IP也可以使用localhost
+    // 主机名
+    host: 'localhost',
+    // compress 服务端压缩是否开启
+    // 开启虚拟服务器时，为你的代码进行压缩。加快开发流程和优化的作用。
+    compress: true,
+    // port 配置服务端口号
+    port: 8899
+  }
 }

492088400be14caa3c675ad99dc6dcee6e0cd867
liuyajun <liuyajun@linhuiba.com>
2019-02-19 11:10:15 +0800

    配置多入口文件打包

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -5,14 +5,15 @@ module.exports = {
   // 入口文件配置项
   entry: {
     // 里面的main是可以随便写的
-    main: './src/main.js'
+    main: './src/main.js',
+    main2: './src/main2.js' // 这里新添加一个入口文件，作用？？？
   },
   //出口文件配置项
   output: {
     // 打包的路径
     path: path.resolve(__dirname, '../dist'),
     // 打包的文件名称
-    filename: 'bundle.js'
+    filename: '[name].js' // 这里name是告诉我们入口进去的是什么名字，打包出来也同样是什么名字
   },
   // 模块：例如解读CSS，图片如何转换，压缩等
   module: {},
